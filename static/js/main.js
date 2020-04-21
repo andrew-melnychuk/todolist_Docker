1091021861966595479b5ae7967e6b5ec1ee6030
andrew-melnychuk <a.melnychuk93@gmail.com>
2020-04-22 00:21:56 +0300

    add class login

--- static/js/main.js
+++ static/js/main.js
@@ -1,37 +1,22 @@
-// List
-
-// let list = document.getElementById('list');
-// list.addEventListener("click", function (e) {
-//   let elem = e.target;
-//   let elemDescription = elem.parentElement.nextElementSibling;
-//   let task = elem.parentElement.parentElement;
-
-//   if (elem.classList.contains('name') && elemDescription != null) {
-//     elemDescription.classList.toggle('display-none');
-//   } else if (elem.classList.contains('delete')) {
-//     task.remove();
-//   }
-// });
-
-// // Arrow btn
-
-// let arrowButton = document.querySelector('#add-item-btn i');
-// arrowButton.addEventListener("click", function (e) {
-//   let elem = e.target;
-//   let addItemForm = document.getElementById('add-item');
-
-//   addItemForm.classList.toggle('display-none');
-//   elem.classList.toggle('fa-chevron-up');
-//   elem.classList.toggle('fa-chevron-down');
-// });
-
-
-
-class LoginPage {
+class MainPage {
   constructor() {
+    this.token = '';
+  }
+
+  setToken(value) {
+    this.token = value;
+  }
+
+  getToken() {
+    return this.token;
+  }
+}
+
+class LoginPage extends MainPage {
+  constructor(token) {
+    super(token);
     this.username = '';
     this.password = '';
-    this.token = '';
     this.id = '';
     this.response = '';
   }
@@ -61,10 +46,12 @@ class LoginPage {
     if (response.ok) {
       let result = await response.json();
       this.token = result.WebAPIToken;
-      // як сюди привязать щоб загружався TaskList це ж має бути метод іншого класу ???
+      console.log(`LoginPage, Token: ${this.token}`);
+
+      // як сюди привязать щоб загружався render(), рядок 189 ???
     } else {
       let result = await response.json();
-      console.log(result.error);
+      console.log(`LoginPage, error: ${result.error}`); //response.error ???
     }
   }
 
@@ -92,7 +79,7 @@ class LoginPage {
       this.render();
     } else {
       let result = await response.json();
-      console.log(result.error);
+      console.log(`LoginPage, error: ${result.error}`); //response.error ???
     }
   }
 
@@ -148,6 +135,167 @@ class LoginPage {
   }
 }
 
-let loginPage = new LoginPage();
+class TaskList extends MainPage {
+  constructor(token) {
+    super(token);
+    this.tasks = new Array();
+  }
 
-loginPage.render();
+  handleEvent(event) {
+    let method = 'on' + event.type[0].toUpperCase() + event.type.slice(1);
+    this[method](event);
+  }
+
+  async getTasks() {
+    let response = await fetch('/api/tasks');
+
+    if (response.ok) {
+      let result = await response.json();
+      this.tasks = result;
+      console.log(`Task List, Tasks${tasks}`);
+      this.render();
+    } else {
+      console.log(`Task List, error ${response.error}`);  //response.error ???
+    }
+  }
+
+  render() {
+    let itemList = `
+    <div id="item-list">
+    <ul id="list">`;
+
+    for (let i = 0; i < this.tasks.length; i++) {
+      itemList += `
+      <li class="item">
+        <div class="item-head flex">
+          <p class="date">${task[i].date}</p>
+          <p class="name">${task[i].name}</p>
+          <i class="fa fa-trash-o delete"></i>
+        </div>
+        <p class="item-body display-none">${task[i].description}</p>
+      </li>`;
+    }
+
+    //   this.tasks.forEach(task => list += `
+    //   <li class="item">
+    //   <div class="item-head flex">
+    //     <p class="date">${task.date}</p>
+    //     <p class="name">${task.name}</p>
+    //     <i class="fa fa-trash-o delete"></i>
+    //   </div>
+    //   <p class="item-body">${task.description}</p>
+    // </li>`);
+
+    itemList += `
+      </ul>
+        <form id="add-item" class="add-item  display-none" method="post">
+          <div class="flex">
+            <input type="text" class="date" placeholder="Date">
+            <input type="text" id="name" placeholder="Name">
+          </div>
+          <div class="flex">
+            <textarea id="description"></textarea>
+            <button type="submit" id="add-btn"><i class="fas fa-plus"></i></button>
+          </div>
+        </form>
+        <button id="add-item-btn" data-action="arrowBtn"><i class="fas fa-chevron-down"></i></button>
+      </div>`;
+
+    let elem = document.getElementById('content');
+    elem.innerHTML = list;
+
+    let arrowButton = document.querySelector('#add-item-btn i');
+    arrowButton.addEventListener("click", taskList);
+
+    let list = document.getElementById('list');
+    list.addEventListener("click", taskList);
+  }
+
+  addTask(task) {
+    let taskDate = task.date;
+    let taskName = task.name;
+    let taskDescription = task.description;
+
+    async () => {
+      let body = {
+        WebAPIToken: this.token,
+        date: taskDate,
+        name: taskName,
+        description: taskDescription
+      }
+
+      let response = await fetch('/api/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+      });
+
+      if (response.ok) {
+        let result = await response.json();
+        this.tasks.push(result);
+        console.log(`Task List, add task message: ${result.message}`);  //response.message ???
+      } else {
+        let result = await response.json();
+        console.log(`Task List, add task error: ${result.error}`);  //response.error ???
+      }
+    }
+  }
+
+  item() {
+    let elem = event.target;
+    let elemDescription = elem.parentElement.nextElementSibling;
+    let task = elem.parentElement.parentElement;
+
+    if (elem.classList.contains('name') && elemDescription != null) {
+      elemDescription.classList.toggle('display-none');
+    } else if (elem.classList.contains('delete')) {
+      async () => {
+        let body = {
+          WebAPIToken: this.token,
+          task_id: idexapmle // change this
+        }
+
+        let response = await fetch('/api/tasks/taskid', {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(body)
+        });
+
+        if (response.ok) {
+          let result = await response.json();
+          console.log(`Task List, delete task message: ${result.message}`);  //response.message ???
+        } else {
+          let result = await response.json();
+          console.log(`Task List, error: ${result.error}`);  //response.error ???
+        }
+      }
+
+      task.remove();
+    }
+  }
+
+  arrowBtn() {
+    let elem = event.target;
+    let addItemForm = document.getElementById('add-item');
+
+    addItemForm.classList.toggle('display-none');
+    elem.classList.toggle('fa-chevron-up');
+    elem.classList.toggle('fa-chevron-down');
+  }
+
+  onClick() {
+    let action = event.target.dataset.action;
+    if (action) {
+      this[action]();
+    }
+  }
+
+}
+
+let main = new MainPage();
+let loginPage = new LoginPage();
+let taskList = new TaskList();
